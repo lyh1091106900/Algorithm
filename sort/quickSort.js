75682569ba4578576ec23488b5c3de26086cc241
lyh1091106900 <lyh1091106900@gmail.com>
2019-03-14 13:54:48 +0800

    quickSort Api

---
+++ sort/quickSort.js
@@ -0,0 +1,59 @@
+/**
+ * 快速排序 
+ * 优点：节省内存 比较次数NlogN 使用率很高的一种排序
+ * 缺点：不稳定，两种条件依次排序会有干扰
+ */
+var tool = require("./utils/tool");
+/**
+ * 
+ * @param {*} a Array 
+ * @param {*} lowIndex int lowIndex of a
+ * @param {*} highIndex int highIndex of a
+ * @param {*} type int 0 for Asc 1 for Dec 
+ */
+function partition(a,lowIndex,highIndex,type=0){
+        var i = lowIndex;
+        var j = highIndex+1;
+        var v = a[lowIndex];
+        while(true){
+            if(type==1)
+            {
+                while(tool.less(a[++i],v)>0) if(i==highIndex) break;
+                while(tool.less(v,a[--j])>0) if(j==lowIndex) break;
+                if(i>=j) break;
+                tool.exch(a,i,j);
+            }
+            if(type==0)
+            {
+                while(tool.less(a[++i],v)<0) if(i==highIndex) break;
+                while(tool.less(v,a[--j])<0) if(j==lowIndex) break;
+                if(i>=j) break;
+                tool.exch(a,i,j);
+            }
+        }
+        tool.exch(a,lowIndex,j);
+        return j;
+}
+/**
+ * 
+ * @param {*} a Array
+ * @param {*} lowIndex int lowIndex of a
+ * @param {*} highIndex int highIndex of a
+ * @param {*} type int 0 for Asc 1 for Dec 
+ */
+function quickSort(a,lowIndex,highIndex,type=0){
+   if(lowIndex>=highIndex) return;
+   var j = partition(a,lowIndex,highIndex,type)
+   quickSort(a,lowIndex,j-1,type);
+   quickSort(a,j+1,highIndex,type);
+}
+/**
+ * 
+ * @param {*} a Array
+ * @param {*} type int 0 for Asc 1 for Dec 
+ */
+function sort(a,type=0){
+    quickSort(a,0,a.length-1,type);
+    return a;
+}
+module.exports.quickSort =sort;
--- test/sortTest.js
+++ test/sortTest.js
@@ -2,6 +2,7 @@ const assert = require('assert')
 var selectSort = require('../sort/selectSort').selectSort;
 var insertSort = require('../sort/insertSort').insertSort;
 var mergeSort = require('../sort/mergeSort').mergeSort;
+var quickSort = require('../sort/quickSort').quickSort;
 describe('selectSort',()=>{
     it('selectSort should return [1,2,3]',()=>{
         assert.equal(2,selectSort([1,3,2])[1])
@@ -15,8 +16,18 @@ describe('insertSort',()=>{
 })
 
 describe('mergeSort',()=>{
-    it('insertSort should return [3, 4, 5, 6, 7, 9]',()=>{
+    it('mergeSort should return [3, 4, 5, 6, 7, 9]',()=>{
         var b =[5,3,4,6,7,9];
         assert.equal(4,mergeSort(b)[1])
+    })
+})
+describe('quickSort',()=>{
+    it('quickSort should return [3, 4, 5, 6, 7, 9]',()=>{
+        var b =[5,3,4,6,7,9];
+        assert.equal(4,quickSort(b)[1])
+    })
+    it('quickSort should return [9,7,6,5,4,3]',()=>{
+        var b =[5,3,4,6,7,9];
+        assert.equal(7,quickSort(b,1)[1])
     })
 })
